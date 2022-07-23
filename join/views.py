de20b69a743cc966ec157c57ee7a0dbe06682384
hyemin <sunsun0721@yonsei.ac.kr>
2022-07-23 16:01:40 +0900

    hyemin

--- join/views.py
+++ join/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def joinpw(request):
+    if request.method == 'PASSWORD':
+        pass
+    else:
+        return render(request,'joinall.html')
