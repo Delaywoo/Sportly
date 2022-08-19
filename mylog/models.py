1c091c53ce4792a558a87610b1932035f6f3988e
jian <hija0608@naver.com>
2022-08-19 21:50:10 +0900

    jian

--- mylog/models.py
+++ mylog/models.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 # Create your models here.
 class Mylog(models.Model):
     writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)#writer=...(, 향후 User와 연결시 작성 예정.)
-    log_date2=models.DateTimeField(default=timezone.now )
+    log_date2=models.DateField(default=timezone.now )
     #mood=models.IntegerField() #각 이모티콘이 1~5의 값을 가짐.
     MOOD_CHOICES = (
         ('😡', '😡'),
@@ -45,34 +45,4 @@ class Comment(models.Model):
     def __str__(self):
         return self.comment #제목을 title 키의 value로. 이 함수는 db를 변경시켜주는 것은 아니라서 migrate 안해줘도 된다.
 
-class Schedule(models.Model):
-    writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    title = models.CharField(max_length=300)
-    date = models.DateField()
-    def __str__(self):
-        return self.title #제목을 title 키의 value로. 이 함수는 db를 변경시켜주는 것은 아니라서 migrate 안해줘도 된다.
-
-    """
-    COLOR_CHOICES=(
-        ('red','red'),
-        ('orange','orange'),
-        ('yellow','yellow'),
-        ('green','green'),
-        ('blue','blue'),
-        ('purple','purple')
-    )"""
-    #color = models.TextField( choices = COLOR_CHOICES,default='red')
-    
-    
-    #color = models.ColorField.(default='#FF0000')
-"""
-class ColorField(models.CharField):
-    def __init__(self, *args, **kwargs):
-        kwargs['max_length'] = 10
-        super(ColorField, self).__init__(*args, **kwargs)
-
-    def formfield(self, **kwargs):
-        kwargs['widget'] = ColorWidget
-        return super(ColorField, self).formfield(**kwargs)
-        """
 
