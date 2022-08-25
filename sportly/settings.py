e3ff54072add39e278a1442c362f7c1b7677ba21
jyw0115 <jyw0115@yonsei.ac.kr>
2022-08-25 15:19:21 +0900

    add files for deploying to heroku

--- sportly/settings.py
+++ sportly/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1$lu%$umg$ldex^njils#qj@4w6x5qi*xa$j1@%cc&*%ctld$i'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'django-insecure-1$lu%$umg$ldex^njils#qj@4w6x5qi*xa$j1@%cc&*%ctld$i')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG', 'True') != 'False')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     'myteam',
     'join',
     'colorfield',
+    'storages',
 ]
 
 #회원가입 기존 내장 User custom
@@ -52,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,3 +146,24 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os #media를 사용하기 위해
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')#파일이 저장될 경로.
 MEDIA_URL = '/media/'#사용자에 의해 올라온 파일에 접근할 수 있는 url 경로
+
+
+# Access key ID: AKIAWCCWMND7BZU7YXFQ
+# Secret across key: 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
+
+AWS_ACCESS_KEY_ID = 'AKIAWCCWMND7BZU7YXFQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_REGION = 'ap-northeast-2'
+AWS_STORAGE_BUCKET_NAME = 'mysportly'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+AWS_DEFAULT_ACL = 'public-read'
+
+STATICFILES_LOCATION = 'static'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
